d0b5af1c3ded6b597442bd73696aa070f37b5909
zllisme <1973842936@qq.com>
2019-08-12 14:20:37 +0800

    test: add test case about statement method when customer has no rental and one regular movie for one day

---
+++ src/test/java/rentalstore/CustomerTest.java
@@ -0,0 +1,30 @@
+package rentalstore;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    private Customer customer = new Customer("Jerry");
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_no_rental() {
+        String statement = customer.statement();
+        assertEquals("Rental Record for Jerry\nAmount owed is 0.0\nYou earned 0 frequent renter points", statement);
+    }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_1_day() {
+        Movie regularMovie = new Movie("test", 0);
+        Rental oneDayRental = new Rental(regularMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + regularMovie.getTitle() + "\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+}
