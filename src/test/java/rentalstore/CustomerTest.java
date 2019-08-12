daa9213c2dfba519e62d94c15ed28f6b3aad0fe5
zllisme <1973842936@qq.com>
2019-08-12 14:38:21 +0800

    test: add two test case about statement method when customer has one child movie for one day and more than three day

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -44,7 +44,7 @@ public class CustomerTest {
 
     @Test
     public void should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_1_day() {
-        Movie newReleaseMovie = new Movie("Titanic", 1);
+        Movie newReleaseMovie = new Movie("test", 1);
         Rental oneDayRental = new Rental(newReleaseMovie, 1);
         customer.addRental(oneDayRental);
 
@@ -52,6 +52,48 @@ public class CustomerTest {
 
         assertEquals("Rental Record for Jerry\n" +
                 "\t" + newReleaseMovie.getTitle() + "\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_more_than_one_day() {
+        Movie newReleaseMovie = new Movie("test", 1);
+        Rental twoDayRental = new Rental(newReleaseMovie, 2);
+        customer.addRental(twoDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + newReleaseMovie.getTitle() + "\t6.0\n" +
+                "Amount owed is 6.0\n" +
+                "You earned 2 frequent renter points", statement);
+    }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_child_movie_for_1_day() {
+        Movie childrenMovie = new Movie("test", 2);
+        Rental oneDayRental = new Rental(childrenMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + childrenMovie.getTitle() + "\t1.5\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_child_movie_for_more_than_three_day() {
+        Movie childrenMovie = new Movie("test", 2);
+        Rental fourDayRental = new Rental(childrenMovie, 4);
+        customer.addRental(fourDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + childrenMovie.getTitle() + "\t3.0\n" +
                 "Amount owed is 3.0\n" +
                 "You earned 1 frequent renter points", statement);
     }
