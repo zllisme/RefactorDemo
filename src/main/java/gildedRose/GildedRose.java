e4427262cd8f137bb6cc8021fd033be7dd80fb2e
zllisme <1973842936@qq.com>
2019-08-12 21:45:55 +0800

    Refactor: move swith logic from cildedRose class to item class

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -8,69 +8,7 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            switch (items[i].name) {
-                case "Aged Brie" :
-                    agedBrieUpdate(items[i]);
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert" :
-                    backstageUpdate(items[i]);
-                    break;
-                case "Sulfuras, Hand of Ragnaros" :
-                    sulfurasUpdate(items[i]);
-                    break;
-                default:
-                    normalUpdate(items[i]);
-            }
+            items[0].updateQuality();
         }
-    }
-
-    public void normalUpdate(Item item) {
-        int quality = item.quality;
-        if(quality > 0) {
-            item.quality = item.quality - 1;
-        }
-        item.sellIn = item.sellIn - 1;
-        if(item.sellIn < 0) {
-            if(quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        }
-    }
-
-    public void agedBrieUpdate(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-        item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
-
-        }
-    }
-    public void backstageUpdate(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
-        item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0) {
-            item.quality = item.quality - item.quality;
-        }
-    }
-
-    public void sulfurasUpdate(Item item) {
-
     }
 }
--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -30,4 +30,69 @@ public class Item {
     public int getQuality() {
         return quality;
     }
+
+    public void updateQuality() {
+        switch (name) {
+            case "Aged Brie" :
+                agedBrieUpdate();
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert" :
+                backstageUpdate();
+                break;
+            case "Sulfuras, Hand of Ragnaros" :
+                sulfurasUpdate();
+                break;
+            default:
+                normalUpdate();
+        }
+    }
+
+    public void normalUpdate() {
+        if(quality > 0) {
+            quality = quality - 1;
+        }
+        sellIn = sellIn - 1;
+        if(sellIn < 0) {
+            if(quality > 0) {
+                quality = quality - 1;
+            }
+        }
+    }
+
+    public void agedBrieUpdate() {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+        sellIn = sellIn - 1;
+        if (sellIn < 0) {
+            if (quality < 50) {
+                quality = quality + 1;
+            }
+
+        }
+    }
+    public void backstageUpdate() {
+        if (quality < 50) {
+            quality = quality + 1;
+            if (sellIn < 11) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+
+            if (this.sellIn < 6) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+        sellIn = sellIn - 1;
+        if (sellIn < 0) {
+            quality = quality - quality;
+        }
+    }
+
+    public void sulfurasUpdate() {
+
+    }
 }
